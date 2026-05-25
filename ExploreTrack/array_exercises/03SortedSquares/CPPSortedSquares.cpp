01b16774383733a78f0c267902148fdf4b7fefaa
siyer20 <90734516+siyer20@users.noreply.github.com>
2026-05-24 23:42:59 -0400

    Created C++ solution file

---
+++ ExploreTrack/array_exercises/03SortedSquares/CPPSortedSquares.cpp
@@ -0,0 +1,43 @@
+
+//This is the 0(n) approach. runtime = 0ms; memory = 30.4mb
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {//[-7,-3,2,3]
+        vector<int> sq(nums.size()); //sq = [0,0,0,0,0]
+        
+        int si = 0;
+        int ei = nums.size()-1;
+        int c = 1;
+        
+        while(c <=nums.size()){
+            int s = nums[si]*nums[si];
+            int e = nums[ei]*nums[ei];
+            
+            if (e>s){
+                sq[sq.size()-c]=e;
+                ei--;
+                c++;
+            }
+            if(e<s){
+                sq[sq.size()-c]=s;
+                si++;
+                c++;
+            }
+            if(e==s){
+                if(ei!=si){
+                    sq[sq.size()-c]=e;
+                    sq[sq.size()-(c+1)]=s;
+                    c+=2;
+                    si++;
+                    ei--;
+                }
+                else{
+                    sq[sq.size()-c]=e;
+                    c++;
+                }
+            }
+        }
+        return sq;
+        
+    }
+};
